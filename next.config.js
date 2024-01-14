19da5051d1923bd96d658576cef23c10ee4bde69
[A19] 김진영 <a44428785@gmail.com>
2024-01-14 21:35:20 +0900

    fix: next.config.js에 quill 설정 추가

--- next.config.js
+++ next.config.js
@@ -3,4 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  // 다른 설정들...
+
+  // webpack 설정 추가
+  webpack: (config, { isServer }) => {
+    // 서버 사이드 렌더링 중에는 Quill을 로드하지 않도록 설정
+    if (isServer) {
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^quill$/,
+        })
+      );
+    }
+    return config;
+  },
+};
