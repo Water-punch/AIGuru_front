52f7280650eb5b5db1f91d30f87a5522c1b80cc1
[AI9] 전성혜01~ <dvlpsh99@gmail.com>
2024-01-29 22:19:25 +0900

    Design: 댓글 CSS 수정:

--- src/components/features/comment/Comment.tsx
+++ src/components/features/comment/Comment.tsx
@@ -16,7 +16,7 @@ const Comment = (commentData: CommentProps) => {
   const deleteComment = useDeleteComment(commentId);
 
   const handleDelete = async () => {
-    deleteComment.mutate();
+    deleteComment.mutate(commentId);
     if (deleteComment.isSuccess && deleteComment.data) {
       console.log('댓글 삭제 성공');
     }
@@ -24,39 +24,39 @@ const Comment = (commentData: CommentProps) => {
 
   if (deletedAt) {
     return (
-      <div className="bg-white p-4 m-1 border-y-2 border-red-300">
+      <div className="bg-red-50 p-4 m-1 border-y-2 border-red-100">
         <p className="text-gray-500 italic">삭제된 댓글입니다</p>
       </div>
     );
   }
 
   return (
-    <div className="bg-gray-100 p-4 m-2 rounded-md border-b-2 border-black">
-      <div className="flex flex-row gap-3">
-        {position && position == 'positive' && (
-          <img
-            src="/images/happy.png"
-            className="rounded-full h-[26px] w-[26px]"
-          ></img>
-        )}
-        {position && position == 'negative' && (
-          <img
-            src="/images/angry.png"
-            className="rounded-full h-[26px] w-[26px]"
-          ></img>
-        )}
-        <p className="text-gray-800 font-semibold">{`익명${anonymous_number}`}</p>
+    <div className="bg-red-50 p-4 m-1 border-y-2 border-red-100">
+      <div className="flex flex-col gap-3">
+        <div className="flex flex gap-3">
+          {position && position == 'positive' && (
+            <img
+              src="/images/happy.png"
+              className="rounded-full h-[26px] w-[26px]"
+            ></img>
+          )}
+          {position && position == 'negative' && (
+            <img
+              src="/images/angry.png"
+              className="rounded-full h-[26px] w-[26px]"
+            ></img>
+          )}
+          <p className="inline-block text-gray-800 font-semibold">{`익명${anonymous_number}`}</p>
+
+          <p className="text-gray-700">{position}</p>
+          <button onClick={handleDelete}>삭제</button>
+        </div>
         <p className="text-gray-700">{content}</p>
-        <p className="text-gray-700">{position}</p>
       </div>
     </div>
   );
 };
 
 export default Comment;
-
-// 삭제한 댓글에는 삭제버튼이 안보이도록.
-// 댓글 삭제시 상태 바로 반영(리로드)
-// 삭제시 익명 없어지게
 
 //<button onClick={handleSubmit}>신고</button>
