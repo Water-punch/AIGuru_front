9c40065a7f8de87b4c80f9e33a8db584db0146fa
[A19] 김진영 <a44428785@gmail.com>
2024-02-02 15:06:45 +0900

    delete: 불필요한 파일 삭제, 주석 제거(일부)

--- src/components/features/board/BoardCardDetail.tsx
+++ src/components/features/board/BoardCardDetail.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 import BoardEdit from './BoardEdit';
 import React from 'react';
 import DOMPurify from 'dompurify';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/src/store';
 
 //백엔드 통신 관련 임시코드
 import axios from 'axios';
 const serverUrl = 'http://localhost:5001/api';
 
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/src/store';
 const api = axios.create({
   baseURL: serverUrl,
   headers: { 'Content-Type': 'application/json' },
